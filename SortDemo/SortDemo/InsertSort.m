4aeb40d10381341024f3a8aa76b76bfe2afb78c7
何欣宇 <cdhexinyu@jd.com>
2014-09-05 16:47:34 +0800

    插入排序算法优化

--- SortDemo/SortDemo/InsertSort.m
+++ SortDemo/SortDemo/InsertSort.m
@@ -17,23 +17,25 @@
     NSLog(@"排序前:%@",numbers);
     
     {
-        NSNumber *temp;
-        int pos;//当前循环，最小值的索引
+        NSNumber *temp;//插入排序的核心
+        int pos;
         for (int i = 1; i < numbers.count; i ++)
         {
-            temp = numbers[i];
+            temp = numbers[i];//取出temp
             pos = i - 1;
             while (pos >= 0 && temp.intValue < [numbers[pos] intValue])
             {
                 @autoreleasepool {
-                    NSNumber *number = numbers[pos+1];
-                    [numbers replaceObjectAtIndex:pos+1 withObject:numbers[pos]];
-                    [numbers replaceObjectAtIndex:pos withObject:number];
+                    
+                    //与当前循环头两个数交换，效率低下
+//                    NSNumber *number = numbers[pos+1];
+//                    [numbers replaceObjectAtIndex:pos+1 withObject:numbers[pos]];
+//                    [numbers replaceObjectAtIndex:pos withObject:number];
+                    numbers[pos+1] = numbers[pos];
                     pos --;
                 }
             }
-            
-            numbers[pos+1] = temp;
+            numbers[pos+1] = temp;//将temp插入到正确位置
         }
     }
     
